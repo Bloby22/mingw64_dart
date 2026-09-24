36a2c0d29dc455e2a8bbcde2dab4de67e6dc9100
Michal <blobycz@proton.me>
2026-09-24 20:36:35 +0200

    Delete include/Services/release_client.h

--- include/Services/release_client.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-#include "Models/release.h"
-
-namespace Services {
-    // Find latest release for channel
-    Models::Release LatestRelease(const std::string& channel = "stable");
-}
